7103a21d9e9dc4cbf84e3c1330656136002a30eb
Renato Santana <renatodev@icloud.com>
2021-09-27 08:14:02 -0300

    removed footer and added header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import './App.css';
-import logo from './logo.svg';
-import Create from './components/Create';
-import Read from './components/Read';
-import Update from './components/Update';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Footer from './Footer';
+import "./App.css";
+import logo from "./logo.svg";
+import Create from "./components/Create";
+import Read from "./components/Read";
+import Update from "./components/Update";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HeaderComponent from "./HeaderComponent";
 
 function App() {
   return (
     <Router>
+    <div>
+    <HeaderComponent />
+    </div>
       <div className="main">
+        
         <h2 className="main-header">React Crud Operations</h2>
         <img src={logo} className="main-logo" alt="logo" />
         <div>
-          <Route exact path='/create' component={Create} />
+          <Route exact path="/create" component={Create} />
         </div>
         <div style={{ marginTop: 20 }}>
-          <Route exact path='/read' component={Read} />
+          <Route exact path="/read" component={Read} />
         </div>
         <div>
-          <Route exact path='/update' component={Update} />
+          <Route exact path="/update" component={Update} />
         </div>
-        <Footer />
       </div>
     </Router>
   );
---
+++ src/HeaderComponent.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { Header, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+export default function HeaderComponent() {
+  return (
+    <div className="header-component">
+      <div class="ui secondary pointing menu">
+        <Header as="h2" color="blue">
+          <Icon loading name="spinner" />
+          <Header.Content>
+            Learning React
+            <Header.Subheader>React CRUD using Axios</Header.Subheader>
+          </Header.Content>
+        </Header>
+        <div class="right menu">
+          <Link to="/create">
+            <a class="active item">Create</a>
+          </Link>
+          <Link to="/read">
+            <a class="item">Read</a>{" "}
+          </Link>
+          <Link to="/update">
+            <a class="item">Update</a>{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
